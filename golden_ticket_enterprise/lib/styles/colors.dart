7c7a9168b6f6a88116c1c160ba2028a74f45b2c1
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-03-18 17:00:05 +0800

    feat[Color]: Color Scheme
    - added colors

---
+++ golden_ticket_enterprise/lib/styles/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart' show Color, Colors;
+
+const Color kSurface = Colors.white;
+const Color kOnSurface = Color(0xFF1A243B);
+
+const Color kPrimary = Color(0xFFC6A663);
+const Color kOnPrimary = Colors.white;
+
+const Color kSecondary = Color(0xFFC1C5CE);
+const Color kOnSecondary = Colors.black;
+
+const Color kTertiary = Color(0xFF8385F3);
+const Color kOnTertiary = Colors.black;
+
+const Color kError = Color(0xFFDB3A3A);
+const Color kOnError = Colors.black;
+
+const Color kPrimaryContainer = Color(0xFFEEDBB2);
+const Color kOnPrimaryContainer = Color(0xFF3E2A00);
+
+const Color kSecondaryContainer = Color(0xFFE4E7F0);
+const Color kOnSecondaryContainer = Color(0xFF181E2C);
+
+const Color kTertiaryContainer = Color(0xFFC3C9F3);
+const Color kOnTertiaryContainer = Color(0xFF1A1742);
+
+const Color kOutline = Color(0xFF838383);
+const Color kOutlineVariant = Color(0xFFD9D9D9);

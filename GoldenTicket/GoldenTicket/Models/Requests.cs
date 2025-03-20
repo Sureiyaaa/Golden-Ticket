eba6e0277e8d067dd6af9ed4a0801f671c6455b2
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-03-20 17:29:07 +0800

    misc: Request Model
    - added required bodies

--- GoldenTicket/GoldenTicket/Models/Requests.cs
+++ GoldenTicket/GoldenTicket/Models/Requests.cs
@@ -1,16 +1,26 @@
-using GoldenTicket.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoldenTicket.Models
 {
-    public class RegisterRequest {
-        public string? username {get;set;} = "";
-        public string? password {get;set;} = "";
-        public string? firstName {get;set;} = "";
-        public char middleInitial {get;set;}
-        public string? lastName {get;set;} = "";
-        
-    }
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string? username { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        public string? password { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string? firstName { get; set; }
+
+        public char? middleInitial { get; set; } // Optional
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string? lastName { get; set; }
+
+        [Required(ErrorMessage = "Role ID is required")]
+        public int? roleID { get; set; }
+    }
     public class LoginRequest {
         public string? username {get;set;}
         public string? password {get;set;}

53091123bb8ded0b38bbe09b11c7936aac575303
Arar <jhericomedina213@gmail.com>
2025-03-21 14:13:35 +0800

    misc: DBUtil.cs
    - added GetTags

--- GoldenTicket/GoldenTicket/Utilities/DBUtil.cs
+++ GoldenTicket/GoldenTicket/Utilities/DBUtil.cs
@@ -1,6 +1,7 @@
 using GoldenTicket.Database;
 using GoldenTicket.Entities;
 using GoldenTicket.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoldenTracker.Models
@@ -25,23 +26,53 @@ namespace GoldenTracker.Models
                 Context.SaveChanges();
             }
         }
-        public static void AddFAQ(string TicketTitle, string Description, MainTag MainTag, SubTag SubTag)
+        #region FAQ
+        public static List<FAQ> GetFAQ()
+        {
+            using(var context = new ApplicationDbContext()){
+                var faqs = context.Faq.ToList();
+                if(faqs.Count == 0 )
+                    throw new Exception("No FAQs found");
+                return faqs;
+            }
+        }
+        public static void AddFAQ(string _title, string _description, string _solution, int _mainTagID, int _subTagID)
         {
             using(var context = new ApplicationDbContext()){
                 var newFAQ = new FAQ
                 {
-                    TicketTitle = TicketTitle,
-                    Description = Description,
+                    Title = _title,
+                    Description = _description,
+                    Solution = _solution,
                     CreatedAt = DateTime.Now,
                     IsArchived = false,
-                    MainTag = MainTag,
-                    SubTag = SubTag
+                    MainTagID = _mainTagID,
+                    SubTagID = _subTagID,
+                    MainTag = context.MainTag.Include(m => m.ChildTags).FirstOrDefault(tag => tag.TagID == _mainTagID),
+                    SubTag = context.SubTag.Include(m => m.MainTag).FirstOrDefault(tag => tag.TagID == _subTagID && tag.MainTagID == _mainTagID)
                 };
                 context.Faq.Add(newFAQ);
                 context.SaveChanges();
             }
-
         }
+        #endregion
+        public static List<T> GetTags<T>() where T : class {
+            using(var context = new ApplicationDbContext()){
+            
+            var maintags = context.MainTag.Include(m => m.ChildTags).Select(m => new {
+                mainTagID = m.TagID,
+                mainTagName = m.TagName,
+                subTags = m.ChildTags.Select(c => new 
+                {
+                subTagID = c.TagID,
+                subTagName = c.TagName,
+                mainTagName = m.TagName
+                }).ToList()
+            }).ToList();
+            return maintags.Cast<T>().ToList();
+            }
+        } 
+        
         public static bool IsUserExisting(string username)
         {
             using(var context = new ApplicationDbContext()){

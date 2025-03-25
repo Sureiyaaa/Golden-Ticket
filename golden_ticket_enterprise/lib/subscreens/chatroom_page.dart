9fb8160d0f15143d4fbdc3647dcf9c8cff5caf4a
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-03-25 10:07:41 +0800

    feat: Provider, Hive
    - Improved Hive initialization
    - Improved Provider Handling
    - Removed SignalrR as provider
    - SignalR is now dependent on DataManager

---
+++ golden_ticket_enterprise/lib/subscreens/chatroom_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:golden_ticket_enterprise/models/data_manager.dart';
+import 'package:golden_ticket_enterprise/models/chatroom.dart';
+import 'package:golden_ticket_enterprise/styles/colors.dart';
+import 'package:provider/provider.dart';
+
+import '../models/hive_session.dart';
+
+class ChatroomListPage extends StatelessWidget {
+  final HiveSession? session;
+  const ChatroomListPage({super.key, required this.session});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<DataManager>(
+      builder: (context, dataManager, child) {
+        List<String> chatrooms = dataManager.chatrooms; // Replace with actual list
+
+        return Scaffold(
+          backgroundColor: kSurface,
+          body: chatrooms.isEmpty
+              ? Center(
+            child: Text(
+              "No chatrooms available",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          )
+              : ListView.builder(
+            itemCount: chatrooms.length,
+            itemBuilder: (context, index) {
+              String chatroom = chatrooms[index];
+              return Card(
+                margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                child: ListTile(
+                  leading: CircleAvatar(child: Icon(Icons.chat_bubble_outline)),
+                  title: Text(chatroom, style: TextStyle(fontWeight: FontWeight.bold)),
+                  subtitle: Text("Opened by: Someone"),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 18, color: Colors.grey),
+                  onTap: () => context.go("/hub/chatroom/${1}"),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}

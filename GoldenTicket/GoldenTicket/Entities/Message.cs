11d8f827e1ae6472fbb92789060f3d12fce7b668
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-03-26 09:57:09 +0800

    misc: DTO
    - added Message DTO

--- GoldenTicket/GoldenTicket/Entities/Message.cs
+++ GoldenTicket/GoldenTicket/Entities/Message.cs
@@ -26,4 +26,17 @@ namespace GoldenTicket.Entities
         public string? MessageContent { get;set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
+
+    public class MessageDTO{
+        public int? MessageID { get; set; }
+        public string? MessageContent { get; set; } = "";
+        public UserDTO? Sender { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public MessageDTO(Message message){
+            this.MessageID = message.MessageID;
+            this.MessageContent = message.MessageContent;
+            this.Sender = new UserDTO(message.Sender!);
+            this.CreatedAt = message.CreatedAt;
+        }
+    }
 }

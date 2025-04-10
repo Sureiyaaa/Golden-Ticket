009959f7c0f6ccc3a81eb19e5c2f82aa46875eb7
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-04-10 10:34:27 +0800

    misc: Patch
    - tile edit non assigned bug
    - removed debug

--- golden_ticket_enterprise/lib/widgets/ticket_tile_widget.dart
+++ golden_ticket_enterprise/lib/widgets/ticket_tile_widget.dart
@@ -67,7 +67,7 @@ class TicketTile extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       IconButton(icon: const Icon(Icons.chat_bubble_outline, color: Colors.blue), onPressed: onChatPressed),
-                      if(ticket.assigned != null && ticket.assigned?.userID == session!.user.userID) IconButton(icon: const Icon(Icons.edit, color: Colors.orange), onPressed: onEditPressed)
+                      if((ticket.assigned != null && ticket.assigned?.userID == session!.user.userID) || ticket.assigned == null) IconButton(icon: const Icon(Icons.edit, color: Colors.orange), onPressed: onEditPressed)
                     ],
                   ),
                 ],

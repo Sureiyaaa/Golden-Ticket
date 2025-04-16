96369f86d8a4646c4425eb2c5ea46f1b8e0472ee
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-04-16 14:54:35 +0800

    feat: Notification System UI
    - added bell unread count
    - added notification list

--- golden_ticket_enterprise/lib/screens/hub.dart
+++ golden_ticket_enterprise/lib/screens/hub.dart
@@ -2,11 +2,16 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:golden_ticket_enterprise/entities/chatroom.dart';
 import 'package:golden_ticket_enterprise/entities/main_tag.dart';
+import 'package:golden_ticket_enterprise/entities/notification.dart'
+    as notifClass;
 import 'package:golden_ticket_enterprise/models/data_manager.dart';
 import 'package:golden_ticket_enterprise/models/hive_session.dart';
 import 'package:golden_ticket_enterprise/screens/connectionstate.dart';
 import 'package:golden_ticket_enterprise/styles/colors.dart';
+import 'package:golden_ticket_enterprise/screens/notification_page.dart';
+import 'package:golden_ticket_enterprise/widgets/notification_tile_widget.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
@@ -15,26 +20,33 @@ class HubPage extends StatefulWidget {
   final StatefulNavigationShell child;
   final DataManager dataManager;
   List<MainTag> mainTags = [];
-  HubPage({Key? key, required this.session, required this.child, required this.dataManager}) : super(key: key);
+  HubPage(
+      {Key? key,
+      required this.session,
+      required this.child,
+      required this.dataManager})
+      : super(key: key);
 
   @override
   State<HubPage> createState() => _HubPageState();
 }
 
-class _HubPageState extends State<HubPage>{
+class _HubPageState extends State<HubPage> {
+  late DataManager dm;
   late int _selectedIndex; // Track selected index
   bool _isInitialized = false;
-  bool _showNotifications = false;
-  final List<String> _notifications = []; // Replace with your Notification model if needed
 
   @override
-  void initState(){
+  void initState() {
     _selectedIndex = widget.child.currentIndex;
     super.initState();
+    dm = Provider.of<DataManager>(context, listen: false);
+
+    dm.signalRService.addOnReceiveSupportListener(_handleReceiveSupport);
   }
+
   @override
   void didChangeDependencies() {
-
     if (!_isInitialized && !widget.dataManager.signalRService.isConnected) {
       log("HubPage: Initializing SignalR Connection...");
       widget.dataManager.signalRService.initializeConnection(widget.session!);
@@ -43,24 +55,11 @@ class _HubPageState extends State<HubPage>{
     super.didChangeDependencies();
   }
 
-
-
   @override
   void dispose() {
     log("HubPage disposed: $this");
     super.dispose();
   }
-
-  // void _getSessionsBox() async {
-  //   // check if session is still valid
-  //   if (Hive.isBoxOpen('sessionBox')) {
-  //     _sessions = Hive.box<HiveSession>('sessionBox');
-  //
-  //   } else {
-  //     _sessions = await Hive.openBox<HiveSession>('sessionBox');
-  //   }
-  // }
-
 
   void _onDrawerItemTapped(int index) {
     print("Selected Index: $_selectedIndex");
@@ -68,86 +67,160 @@ class _HubPageState extends State<HubPage>{
     setState(() {
       _selectedIndex = index;
     });
-    widget.child.goBranch(index, initialLocation: index == widget.child.currentIndex);
+    widget.child
+        .goBranch(index, initialLocation: index == widget.child.currentIndex);
   }
 
-
+  void _handleReceiveSupport(Chatroom chatroom){
+    context.push('/hub/chatroom/${chatroom.chatroomID}');
+  }
   @override
   Widget build(BuildContext context) {
     if (widget.session == null) {
       return SizedBox.shrink(); // Prevents UI from rendering if redirecting
     }
-    return Consumer<DataManager>(
-        builder: (context, dataManager, child) {
-          if (!dataManager.signalRService.isConnected) {
-            return DisconnectedOverlay();
-          }
-          void _logout() async {
-            var box = Hive.box<HiveSession>('sessionBox');
-            context.go('/login');
-            await box.delete('user'); // Clear session
-            await dataManager.closeConnection();
-          }
-          return Scaffold(
-            backgroundColor: kSurface,
-            appBar: AppBar(
-              backgroundColor: kPrimary,
-              title: Text(_getAppBarTitle()), // Dynamic title
-              actions: [
-                  PopupMenuButton(
-                  icon: Icon(Icons.notifications),
-                  tooltip: "Notifications",
-                  itemBuilder: (context) {
-                    if (_notifications.isEmpty) {
-                      return [
-                        PopupMenuItem(
-                          enabled: false,
-                          child: Text('No notifications'),
-                        )
-                      ];
-                    } else {
-                      return _notifications
-                          .map((notification) => PopupMenuItem(
-                        child: Text(notification),
-                      ))
-                          .toList();
-                    }
-                  },
+    return Consumer<DataManager>(builder: (context, dataManager, child) {
+      if (!dataManager.signalRService.isConnected) {
+        return DisconnectedOverlay();
+      }
+      int unreadCount = dataManager.notifications.where((notif) => !notif.isRead).length;
+
+      void _logout() async {
+        var box = Hive.box<HiveSession>('sessionBox');
+        context.go('/login');
+        await box.delete('user'); // Clear session
+        await dataManager.closeConnection();
+      }
+
+      return Scaffold(
+          backgroundColor: kSurface,
+          appBar: AppBar(
+            backgroundColor: kPrimary,
+            title: Text(_getAppBarTitle()), // Dynamic title
+            actions: [
+              PopupMenuButton(
+                icon: Stack(clipBehavior: Clip.none, children: [
+                  Icon(Icons.notifications),
+                  if (unreadCount > 0)
+                    Positioned(
+                      right: -5,
+                      top: -8,
+                      child: CircleAvatar(
+                        radius: 10,
+                        backgroundColor: Colors.red,
+                        child: Text(
+                          unreadCount.toString(),
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                ]),
+                tooltip: "Notifications",
+                itemBuilder: (context) {
+                  List<PopupMenuEntry> items = [];
+
+                  items.add(
+                    PopupMenuItem(
+                      enabled: false,
+                      padding: EdgeInsets.zero,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minHeight: 200, // Minimum height
+                          maxHeight: 400, // Maximum height to prevent overflow
+                        ),
+                        child: dataManager.notifications.isEmpty
+                            ? Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Text('No notifications'),
+                                ),
+                              )
+                            : SingleChildScrollView(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: dataManager.notifications.map((notif) {
+                                    return Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5, vertical: 10),
+                                      child: InkWell(
+                                        onTap: () {},
+                                        child: NotificationTile(
+                                            notification: notif),
+                                      ),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                      ),
+                    ),
+                  );
+
+                  items.add(const PopupMenuDivider());
+
+                  items.add(
+                    PopupMenuItem(
+                      enabled: false,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => NotificationsPage(
+                                    notifications: dataManager.notifications),
+                              ),
+                            );
+                          },
+                          child: Text('See all notifications'),
+                        ),
+                      ),
+                    ),
+                  );
+
+                  return items;
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.logout),
+                tooltip: "Logout",
+                onPressed: _logout,
+              ),
+            ],
+          ),
+          drawer: Drawer(
+            backgroundColor: kPrimaryContainer,
+            child: Column(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text(widget.session!.user.username),
+                  accountEmail: Text("Dummy Email"),
+                  decoration: BoxDecoration(color: kPrimary),
+                  currentAccountPicture: CircleAvatar(
+                    child: Icon(Icons.person, size: 40),
+                  ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.logout),
-                  tooltip: "Logout",
-                  onPressed: _logout,
-                ),
+                _buildDrawerItem(Icons.dashboard, "Dashboard", 0),
+                _buildDrawerItem(Icons.message_outlined, "Chatrooms", 1),
+                if (widget.session?.user.role == "Admin" ||
+                    widget.session?.user.role == "Staff")
+                  _buildDrawerItem(Icons.list, "Tickets", 2),
+                _buildDrawerItem(Icons.question_mark, "FAQ", 3),
+                if (widget.session?.user.role != "Employee")
+                  _buildDrawerItem(Icons.show_chart, "Reports", 4),
+                if (widget.session?.user.role == "Admin")
+                  _buildDrawerItem(Icons.person_outline, "User Management", 5),
+                if (widget.session?.user.role == "Admin")
+                  _buildDrawerItem(Icons.settings, "Settings", 6),
               ],
             ),
-            drawer: Drawer(
-              backgroundColor: kPrimaryContainer,
-              child: Column(
-                children: <Widget>[
-                  UserAccountsDrawerHeader(
-                    accountName: Text(widget.session!.user.username),
-                    accountEmail: Text("Dummy Email"),
-                    decoration: BoxDecoration(color: kPrimary),
-                    currentAccountPicture: CircleAvatar(
-                      child: Icon(Icons.person, size: 40),
-                    ),
-                  ),
-                  _buildDrawerItem(Icons.dashboard, "Dashboard", 0),
-                  _buildDrawerItem(Icons.message_outlined, "Chatrooms", 1),
-                  if(widget.session?.user.role == "Admin" || widget.session?.user.role == "Staff") _buildDrawerItem(Icons.list, "Tickets", 2),
-                  _buildDrawerItem(Icons.question_mark, "FAQ", 3),
-                  if(widget.session?.user.role != "Employee") _buildDrawerItem(Icons.show_chart, "Reports", 4),
-                  if(widget.session?.user.role == "Admin") _buildDrawerItem(Icons.person_outline, "User Management", 5),
-                  if(widget.session?.user.role == "Admin") _buildDrawerItem(Icons.settings, "Settings", 6),
-                ],
-              ),
-            ),
-            body: widget.child
-          );
-        }
-    );
-
+          ),
+          body: widget.child);
+    });
   }
 
   /// **Dynamic AppBar Title**
@@ -175,8 +248,11 @@ class _HubPageState extends State<HubPage>{
   /// **Drawer Item Builder with Highlighting**
   Widget _buildDrawerItem(IconData icon, String title, int index) {
     return ListTile(
-      leading: Icon(icon, color: _selectedIndex == index ? Colors.blue : Colors.black),
-      title: Text(title, style: TextStyle(color: _selectedIndex == index ? Colors.blue : Colors.black)),
+      leading: Icon(icon,
+          color: _selectedIndex == index ? Colors.blue : Colors.black),
+      title: Text(title,
+          style: TextStyle(
+              color: _selectedIndex == index ? Colors.blue : Colors.black)),
       tileColor: _selectedIndex == index ? Colors.blue.withOpacity(0.2) : null,
       onTap: () => _onDrawerItemTapped(index),
     );

005969263e0e57c14b8b667824ce96af33ea52f7
Caesar <46262050+Sureiyaaa@users.noreply.github.com>
2025-04-28 17:41:37 +0800

    fix: dashboard page
    - status displaying on center screen

--- golden_ticket_enterprise/lib/subscreens/dashboard_page.dart
+++ golden_ticket_enterprise/lib/subscreens/dashboard_page.dart
@@ -58,113 +58,124 @@ class _DashboardPageState extends State<DashboardPage> {
                   return Scrollbar(
                     controller: _scrollController,
                     thumbVisibility: true,
-                    child: SingleChildScrollView( // Wrap entire body in SingleChildScrollView
+                    child: CustomScrollView( // Changed to CustomScrollView
                       controller: _scrollController,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // **Ticket Overview**
-                          Text("Ticket Overview", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                          SizedBox(height: 10),
+                      slivers: [
+                        // **Ticket Overview**
+                        SliverToBoxAdapter(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Ticket Overview", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                              SizedBox(height: 10),
 
-                          // **Stats Grid (Responsive)**
-                          LayoutBuilder(
-                            builder: (context, constraints) {
-                              return GridView.builder(
-                                shrinkWrap: true,
-                                physics: NeverScrollableScrollPhysics(),
-                                itemCount: ticketCounts.length,
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: columns,
-                                  crossAxisSpacing: 10,
-                                  mainAxisSpacing: 10,
-                                  childAspectRatio: 2.5,
-                                ),
-                                itemBuilder: (context, index) {
-                                  String status = ticketCounts.keys.elementAt(index);
-                                  int count = ticketCounts[status] ?? 0;
-                                  Color color = getStatusColor(status) ?? Colors.grey;
+                              // **Stats Grid (Responsive)**
+                              LayoutBuilder(
+                                builder: (context, constraints) {
+                                  return GridView.builder(
+                                    shrinkWrap: true,
+                                    physics: NeverScrollableScrollPhysics(),
+                                    itemCount: ticketCounts.length,
+                                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: columns,
+                                      crossAxisSpacing: 10,
+                                      mainAxisSpacing: 10,
+                                      childAspectRatio: 2.5,
+                                    ),
+                                    itemBuilder: (context, index) {
+                                      String status = ticketCounts.keys.elementAt(index);
+                                      int count = ticketCounts[status] ?? 0;
+                                      Color color = getStatusColor(status) ?? Colors.grey;
 
-                                  return Card(
-                                    elevation: 2,
-                                    color: color.withOpacity(0.2),
-                                    child: Padding(
-                                      padding: EdgeInsets.symmetric(vertical: 16),
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          CircleAvatar(
-                                            backgroundColor: color,
-                                            child: Text("$count",
-                                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                                      return Card(
+                                        elevation: 2,
+                                        color: color.withOpacity(0.2),
+                                        child: Padding(
+                                          padding: EdgeInsets.symmetric(vertical: 16),
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                              CircleAvatar(
+                                                backgroundColor: color,
+                                                child: Text("$count",
+                                                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                                              ),
+                                              SizedBox(height: 5),
+                                              Flexible(
+                                                  child: Text(status, style: TextStyle(fontSize: 14))
+                                              )
+                                            ],
                                           ),
-                                          SizedBox(height: 5),
-                                          Flexible(
-                                              child: Text(status, style: TextStyle(fontSize: 14))
-                                          )
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        SliverToBoxAdapter(child: SizedBox(height: 20)),
+
+                        // **Recent Tickets**
+                        SliverToBoxAdapter(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Recent Tickets", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                              SizedBox(height: 10),
+
+                              recentTickets.isEmpty
+                                  ? Center(child: Text("No recent tickets"))
+                                  : ListView.builder(
+                                shrinkWrap: true, // Allow it to take only the space needed
+                                physics: NeverScrollableScrollPhysics(), // Avoid conflicts with parent scroll
+                                itemCount: recentTickets.length,
+                                itemBuilder: (context, index) {
+                                  Ticket ticket = recentTickets[index];
+                                  return Card(
+                                    child: ListTile(
+                                      onTap: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) => TicketDetailsPopup(
+                                            ticket: ticket,
+                                            onChatPressed: () => handleChat(dataManager, ticket),
+                                          ),
+                                        );
+                                      },
+                                      leading: Icon(Icons.confirmation_number, color: Colors.blue),
+                                      title: Text("#${ticket.ticketID}: ${ticket.ticketTitle ?? "No title provided"}"),
+                                      subtitle: Row(
+                                        children: [
+                                          Chip(
+                                            label: Text(ticket.priority!,
+                                                style: TextStyle(color: Colors.white, fontSize: 12)),
+                                            backgroundColor: getPriorityColor(ticket.priority!),
+                                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                                          ),
+                                          Chip(
+                                            label: Text(ticket.status,
+                                                style: TextStyle(color: Colors.white, fontSize: 12)),
+                                            backgroundColor: getStatusColor(ticket.status),
+                                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                                          ),
+                                          SizedBox(width: 10),
+                                          Text(
+                                            TimeUtil.formatTimestamp(ticket.createdAt), // Show only date
+                                            style: TextStyle(color: Colors.grey),
+                                          ),
                                         ],
                                       ),
                                     ),
                                   );
                                 },
-                              );
-                            },
+                              ),
+                            ],
                           ),
-
-                          SizedBox(height: 20),
-
-                          // **Recent Tickets**
-                          Text("Recent Tickets", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                          SizedBox(height: 10),
-
-                          recentTickets.isEmpty
-                              ? Center(child: Text("No recent tickets"))
-                              : ListView.builder(
-                            shrinkWrap: true, // Allow it to take only the space needed
-                            physics: NeverScrollableScrollPhysics(), // Avoid conflicts with parent scroll
-                            itemCount: recentTickets.length,
-                            itemBuilder: (context, index) {
-                              Ticket ticket = recentTickets[index];
-                              return Card(
-                                child: ListTile(
-                                  onTap: () {
-                                    showDialog(
-                                      context: context,
-                                      builder: (context) => TicketDetailsPopup(
-                                          ticket: ticket,
-                                        onChatPressed: () => handleChat(dataManager, ticket),
-                                      ),
-                                    );
-                                  },
-                                  leading: Icon(Icons.confirmation_number, color: Colors.blue),
-                                  title: Text("#${ticket.ticketID}: ${ticket.ticketTitle ?? "No title provided"}"),
-                                  subtitle: Row(
-                                    children: [
-                                      Chip(
-                                        label: Text(ticket.priority!,
-                                            style: TextStyle(color: Colors.white, fontSize: 12)),
-                                        backgroundColor: getPriorityColor(ticket.priority!),
-                                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                                      ),
-                                      Chip(
-                                        label: Text(ticket.status,
-                                            style: TextStyle(color: Colors.white, fontSize: 12)),
-                                        backgroundColor: getStatusColor(ticket.status),
-                                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                                      ),
-                                      SizedBox(width: 10),
-                                      Text(
-                                        TimeUtil.formatTimestamp(ticket.createdAt), // Show only date
-                                        style: TextStyle(color: Colors.grey),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   );
                 },
@@ -174,8 +185,8 @@ class _DashboardPageState extends State<DashboardPage> {
         ),
       ),
     );
-
   }
+
   void handleChat(DataManager dataManager, Ticket ticket){
     try {
       openChatroom(context, widget.session!, dataManager, dataManager.findChatroomByTicketID(

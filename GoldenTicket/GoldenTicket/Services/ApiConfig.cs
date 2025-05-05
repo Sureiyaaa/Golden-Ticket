0e700805cb7e754def93976a5a8718362e4a19a6
Arar <jhericomedina213@gmail.com>
2025-05-05 16:57:28 +0800

    Update ApiConfig.cs

--- GoldenTicket/GoldenTicket/Services/ApiConfig.cs
+++ GoldenTicket/GoldenTicket/Services/ApiConfig.cs
@@ -33,13 +33,16 @@ public class ApiConfig
     public async Task<APIKeyDTO> GetLeastUsedAPI(int lastID = 0,int index = 0)
     {
         OpenAIKeys = await DBUtil.GetAPIKeys();
+        if( OpenAIKeys == null || OpenAIKeys.Count == 0)
+        {
+            if (OpenAIKeys == null || OpenAIKeys.Count == 0)
+            _logger.LogWarning("[ApiConfig] [ERROR] OpenAIKeys is not initialized or is empty.");
+            return null!;
+        }
         AvailableKeys = OpenAIKeys.Where(a => a.LastRateLimit < DateTime.UtcNow.AddHours(-24)).ToList();
         LeastUsedKeys = OpenAIKeys.Where(a => a.LastRateLimit < DateTime.UtcNow.AddHours(-24)).OrderBy(a => a.Usage).ToList();
 
         APIKeyDTO leastUsedKeyEntity;
-
-        if (OpenAIKeys == null || OpenAIKeys.Count == 0)
-            _logger.LogWarning("[ApiConfig] [ERROR] OpenAIKeys is not initialized or is empty.");
         
         leastUsedKeyEntity = LeastUsedKeys.ElementAtOrDefault(index)!;
         if (leastUsedKeyEntity.APIKeyID == lastID && index != 0)
